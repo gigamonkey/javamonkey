11045023c441960b7b939a1398a8e45c36faf02f
Peter Seibel <peter@gigamonkeys.com>
2024-06-15 10:24:38 -0700

    Add MIT license variable.

--- javamonkey-templates.el
+++ javamonkey-templates.el
@@ -333,6 +333,30 @@ java-catch template."
  * USA
  */"))
 
+(defvar javamonkey-mit-java
+  (format-time-string
+"/*
+ * Copyright (c) %Y Peter Seibel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the \"Software\"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */"))
+
 (defvar javamonkey-gpl-lisp
   (format-time-string
 ";;
@@ -355,7 +379,7 @@ java-catch template."
 ;;"))
 
 
-(defvar javamonkey-java-copyright javamonkey-gpl-java)
+(defvar javamonkey-java-copyright javamonkey-mit-java)
 
 (defvar javamonkey-lisp-copyright javamonkey-gpl-lisp)
 
